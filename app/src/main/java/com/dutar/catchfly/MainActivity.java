5117a9944b80a8b74d1e0108c2ec3b683fd3876f
dutarrr <duzguntutar@gmail.com>
2021-01-09 00:10:39 +0300

    admob

--- app/src/main/java/com/dutar/catchfly/MainActivity.java
+++ app/src/main/java/com/dutar/catchfly/MainActivity.java
@@ -2,27 +2,44 @@ package com.dutar.catchfly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.opengl.Visibility;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.AdRequest;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    int score;
-    TextView textScore;
+    TextView textViewHighestScore;
+    TextView textViewScore;
     TextView textViewTime;
-
     ImageView imageView1, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7, imageView8, imageView9, imageView10, imageView11, imageView12;
     ImageView[] AllImages;
+    Button buttonStart;
+
+    CountDownTimer countDownTimer;
 
     Runnable runnable;
     Handler handler;
+    int counterSpeed = 500;
+    int score = 0;
 
+    SharedPreferences sharedPreferences;
+    private final String _highestScore = "HighestScore";
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +48,11 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
+        adMobSettings();
+
         CountDowner();
 
-        otoHideFly();
+        RandomHideFly();
     }
 
     private void init() {
@@ -49,18 +68,54 @@ public class MainActivity extends AppCompatActivity {
         imageView10 = findViewById(R.id.imageView10);
         imageView11 = findViewById(R.id.imageView11);
         imageView12 = findViewById(R.id.imageView12);
-
         AllImages = new ImageView[]{imageView1, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7, imageView8, imageView9, imageView10, imageView11, imageView10, imageView12};
 
-        score = 0;
         textViewTime = findViewById(R.id.textViewTime);
+        textViewScore = findViewById(R.id.textScore);
+        textViewHighestScore = findViewById(R.id.textViewHighestScore);
+        buttonStart = findViewById(R.id.buttonStart);
+        sharedPreferences = this.getSharedPreferences("com.dutar.catchfly", MODE_PRIVATE);
 
-        textScore = findViewById(R.id.textScore);
-        textScore.setText("Skor: " + score);
+        SetDefaultValues();
+
+        HideAllFly();
+
+        CreateAlertDialog("Hoşgeldin :)", "Sinekleri yakalamaya hazırsan \"BAŞLA\" butonuna tıkla ve sinekleri yakala :)", "Tamam", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
     }
 
+    private void SetDefaultValues() {
+        textViewScore.setText("Skor: 0");
+        textViewHighestScore.setText("Rekor: " + GetSharedPreferences(_highestScore));
+    }
+
+    private void adMobSettings() {
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        //InterstitialAd
+        mInterstitialAd = new InterstitialAd(MainActivity.this);
+        mInterstitialAd.setAdUnitId("xxxxxxxxxxxx");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+    }
+
+    //timer
     public void CountDowner() {
-        new CountDownTimer(10000, 1000) {
+        countDownTimer = new CountDownTimer(10000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 textViewTime.setText("Kalan süre: " + millisUntilFinished / 1000);
@@ -69,40 +124,96 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 textViewTime.setText("Kalan süre: 0");
-
-                hideAllFly();
-
                 handler.removeCallbacks(runnable);
+
+                HideAllFly();
+                buttonStart.setVisibility(View.VISIBLE);
+
+                FinishGame();
             }
-        }.start();
+        };
     }
 
-    private void otoHideFly() {
+    //Counter
+    private void RandomHideFly() {
         handler = new Handler();
         runnable = new Runnable() {
             @Override
             public void run() {
-                hideAllFly();
+                HideAllFly();
 
-                Random random = new Random();
-                int index = random.nextInt(AllImages.length);
-                AllImages[index].setVisibility(View.VISIBLE);
-                handler.postDelayed(this, 500);
+                ShowRandomFly();
+
+                handler.postDelayed(this, counterSpeed);
             }
         };
-
-        handler.post(runnable);
     }
 
-    private void hideAllFly() {
+    private void HideAllFly() {
         for (ImageView image : AllImages)
             image.setVisibility(View.INVISIBLE);
     }
 
-    public void fly_Click(View view) {
-        score++;
-        textScore.setText("Skor: " + score);
+    private void ShowRandomFly() {
+        Random random = new Random();
+        int index = random.nextInt(AllImages.length);
+        AllImages[index].setVisibility(View.VISIBLE);
     }
 
+    public void Fly_Click(View view) {
+        score++;
+        textViewScore.setText("Skor: " + score);
+    }
 
+    public void ButtonStart_Click(View view) {
+        score = 0;
+        textViewScore.setText("Skor: " + score);
+        buttonStart.setVisibility(View.INVISIBLE);
+
+        countDownTimer.start();
+        handler.post(runnable);
+    }
+
+    public int GetSharedPreferences(String key) {
+        return sharedPreferences.getInt(key, 0);
+    }
+
+    public void SetSharedPreferences(String key, int value) {
+        sharedPreferences.edit().putInt(key, value).apply();
+    }
+
+    public void FinishGame() {
+        int highestScore = GetSharedPreferences(_highestScore);
+
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                }
+            }
+        };
+
+        if (score > highestScore) {
+            SetSharedPreferences(_highestScore, score);
+            textViewHighestScore.setText("Rekor: " + score);
+
+            CreateAlertDialog("Tebrikler yeni rekor kırdın :)", "Yeni rekor :" + score, "Tamam", listener);
+
+        } else if (score == 0) {
+            CreateAlertDialog("Hiç sinek yakalayamadın :(", "Skor :" + score, "Tekrar Dene", listener);
+
+        } else {
+            CreateAlertDialog("Rekoru kıramadın :( ", "Skor :" + score, "Tekrar Dene", listener);
+        }
+    }
+
+    private void CreateAlertDialog(String title, String message, String positiveButtonText, DialogInterface.OnClickListener positiveButton) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+        alert.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(positiveButtonText, positiveButton).setCancelable(false);
+
+        alert.show();
+    }
 }
